da9da57d61c06ea1a2940a61a5a2a1a194d65bb9
LURKS02 <thecrooks2@gmail.com>
2020-08-11 04:55:25 +0900

    ex03

---
+++ ex03.py
@@ -0,0 +1,19 @@
+
+# list ===============================
+list = []
+list = [0,2,3,4]
+list[0] = 1
+
+
+# list func ==========================
+numlist = []
+
+numlist.append(1)
+numlist.append(2)
+numlist.append(3)
+
+del numlist[0]
+numlist.remove(2)
+
+size = len(numlist)
+print("size = ", size)
